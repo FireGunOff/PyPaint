87e27f1185b0e693deb3607db8c3bd9a90ceca79
Koteika4 <45179902+Koteika4@users.noreply.github.com>
2018-12-13 17:42:32 +0300

    Сделал функцию цвета, размера кисти и рисования"

--- pyPaint.py
+++ pyPaint.py
@@ -11,3 +11,19 @@ class Paint(Frame):
         self.color = "black"
         self.brush_size = 2
         self.setUI()
+        
+    def set_color(self, new_color): 
+    
+        self.color = new_color 
+    
+    def set_brush_size(self, new_size): 
+    
+        self.brush_size = new_size 
+    
+    def draw(self, event): 
+    
+        self.canv.create_oval(event.x - self.brush_size, 
+                              event.y - self.brush_size, 
+                              event.x + self.brush_size, 
+                              event.y + self.brush_size, 
+                              fill=self.color, outline=self.color)     
